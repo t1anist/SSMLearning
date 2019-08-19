b5568d9b8054c64b25c366c21ab7b12f062b677d
Tian <813400513@qq.com>
2019-08-19 17:47:52 +0800

    Employee series tables and POJOs

---
+++ Chapter5Mapper/src/com/chapter5/entity/FemaleHealthForm.java
@@ -0,0 +1,13 @@
+package com.chapter5.entity;
+
+public class FemaleHealthForm extends HealthForm {
+    private String uterus;
+
+    public String getUterus() {
+        return uterus;
+    }
+
+    public void setUterus(String uterus) {
+        this.uterus = uterus;
+    }
+}
---
+++ Chapter5Mapper/src/com/chapter5/entity/MaleHealthForm.java
@@ -0,0 +1,13 @@
+package com.chapter5.entity;
+
+public class MaleHealthForm extends HealthForm {
+    private String prostate;
+
+    public String getProstate() {
+        return prostate;
+    }
+
+    public void setProstate(String prostate) {
+        this.prostate = prostate;
+    }
+}
